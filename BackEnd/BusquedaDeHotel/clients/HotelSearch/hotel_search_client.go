2f6539c00770e4a7a78db516bcb090a7c93756a5
Mussa212 <santimussa21@gmail.com>
2023-11-06 13:39:00 -0300

    Agregar hoteles por ciudad

--- BackEnd/BusquedaDeHotel/clients/HotelSearch/hotel_search_client.go
+++ BackEnd/BusquedaDeHotel/clients/HotelSearch/hotel_search_client.go
@@ -63,107 +63,106 @@ func GetHotelsByDateAndCity(searchDto dto.SearchDto) (dto.HotelsDto, e.ApiError)
 
 	// grab results for ease of use later on
 	results := res.Results
-  var solrDto dto.SolrDto
+	var solrDto dto.SolrDto
 
-
-var hotelsByCity dto.HotelsDto
+	var hotelsByCity dto.HotelsDto
 	var hotelDto dto.HotelDto
 	for i := 0; i < results.Len(); i++ {
-// Asigna el valor del campo "hotel_id" a una variable de tipo interface{}
-hotelIdInterface := results.Get(i).Field("hotel_id")
+		// Asigna el valor del campo "hotel_id" a una variable de tipo interface{}
+		hotelIdInterface := results.Get(i).Field("hotel_id")
 
-// Verifica si el valor es un slice de interfaces
-if hotelIds, ok := hotelIdInterface.([]interface{}); ok {
-    // Inicializa un nuevo slice de strings
-    var hotelIdStrings []string
+		// Verifica si el valor es un slice de interfaces
+		if hotelIds, ok := hotelIdInterface.([]interface{}); ok {
+			// Inicializa un nuevo slice de strings
+			var hotelIdStrings []string
 
-    // Convierte cada elemento a string
-    for _, id := range hotelIds {
-        if strId, ok := id.(string); ok {
-            hotelIdStrings = append(hotelIdStrings, strId)
-        }
-    }
+			// Convierte cada elemento a string
+			for _, id := range hotelIds {
+				if strId, ok := id.(string); ok {
+					hotelIdStrings = append(hotelIdStrings, strId)
+				}
+			}
 
-    // Asigna el nuevo slice de strings a tu variable solrDto.Id
-    solrDto.Id = hotelIdStrings
-} else {
-    // Maneja el caso en que el tipo no sea el esperado
+			// Asigna el nuevo slice de strings a tu variable solrDto.Id
+			solrDto.Id = hotelIdStrings
+		} else {
+			// Maneja el caso en que el tipo no sea el esperado
 			fmt.Println("ID is not a string!")
 			return nil, e.NewBadRequestApiError("Error con un tipo de dato de solr")
-}
+		}
 		hotelDto.Id = solrDto.Id[0]
 
-nameInterface := results.Get(i).Field("name")
-if names, ok := nameInterface.([]interface{}); ok {
-    // Inicializa un nuevo slice de strings
-    var nameStrings []string
+		nameInterface := results.Get(i).Field("name")
+		if names, ok := nameInterface.([]interface{}); ok {
+			// Inicializa un nuevo slice de strings
+			var nameStrings []string
 
-    // Convierte cada elemento a string
-    for _, name := range names {
-        if strName, ok := name.(string); ok {
-            nameStrings = append(nameStrings, strName)
-        }
-    }
+			// Convierte cada elemento a string
+			for _, name := range names {
+				if strName, ok := name.(string); ok {
+					nameStrings = append(nameStrings, strName)
+				}
+			}
 
-    // Asigna el nuevo slice de strings a tu variable solrDto.Id
-    solrDto.Name = nameStrings
-} else {
-    // Maneja el caso en que el tipo no sea el esperado
+			// Asigna el nuevo slice de strings a tu variable solrDto.Id
+			solrDto.Name = nameStrings
+		} else {
+			// Maneja el caso en que el tipo no sea el esperado
 			fmt.Println("Name is not a string!")
 			return nil, e.NewBadRequestApiError("Error con un tipo de dato de solr")
-}
-    fmt.Println(solrDto.Name[0])
+		}
+		fmt.Println(solrDto.Name[0])
 		hotelDto.Name = solrDto.Name[0]
 
-
 		hotelDescInterface := results.Get(i).Field("descripcion")
-    if descs, ok := hotelDescInterface.([]interface{}); ok {
-      // Inicializa un nuevo slice de strings
-      var descStrings []string
+		if descs, ok := hotelDescInterface.([]interface{}); ok {
+			// Inicializa un nuevo slice de strings
+			var descStrings []string
 
-      // Convierte cada elemento a string
-      for _, desc := range descs {
-        if strDesc, ok := desc.(string); ok {
-            descStrings = append(descStrings, strDesc)
-        }
-    }
+			// Convierte cada elemento a string
+			for _, desc := range descs {
+				if strDesc, ok := desc.(string); ok {
+					descStrings = append(descStrings, strDesc)
+				}
+			}
 
-    // Asigna el nuevo slice de strings a tu variable solrDto.Id
-    solrDto.Desc = descStrings
-    } else {
-    // Maneja el caso en que el tipo no sea el esperado
+			// Asigna el nuevo slice de strings a tu variable solrDto.Id
+			solrDto.Desc = descStrings
+		} else {
+			// Maneja el caso en que el tipo no sea el esperado
 			fmt.Println("Descripcion is not a string!")
 			return nil, e.NewBadRequestApiError("Error con un tipo de dato de solr")
-    }
-    fmt.Println(solrDto.Desc[0])
+		}
+		fmt.Println(solrDto.Desc[0])
 		hotelDto.Desc = solrDto.Desc[0]
 
-
 		hotelAmenitiesInterface := results.Get(i).Field("amenities")
-    if amenities, ok := hotelAmenitiesInterface.([]interface{}); ok {
-      // Inicializa un nuevo slice de strings
-      var amenitiesStrings []string
+		if amenities, ok := hotelAmenitiesInterface.([]interface{}); ok {
+			// Inicializa un nuevo slice de strings
+			var amenitiesStrings []string
 
-      // Convierte cada elemento a string
-      for _, amenitie := range amenities {
-        if strAmenitie, ok := amenitie.(string); ok {
-            amenitiesStrings = append(amenitiesStrings, strAmenitie)
-        }
+			// Convierte cada elemento a string
+			for _, amenitie := range amenities {
+				if strAmenitie, ok := amenitie.(string); ok {
+					amenitiesStrings = append(amenitiesStrings, strAmenitie)
+				}
 
-    }
+			}
 
-    // Asigna el nuevo slice de strings a tu variable solrDto.Id
-    solrDto.Amenities = amenitiesStrings
-    } else {
-    // Maneja el caso en que el tipo no sea el esperado
+			// Asigna el nuevo slice de strings a tu variable solrDto.Id
+			solrDto.Amenities = amenitiesStrings
+		} else {
+			// Maneja el caso en que el tipo no sea el esperado
 			fmt.Println("Amenities is not a string!")
 			return nil, e.NewBadRequestApiError("Error con un tipo de dato de solr")
-    }
-    fmt.Println(solrDto.Amenities[0])
+		}
+		fmt.Println(solrDto.Amenities[0])
 		hotelDto.Amenities = solrDto.Amenities
+		hotelDto.Ciudad = searchDto.Ciudad
+
+		hotelsByCity = append(hotelsByCity, hotelDto)
 
 	}
-  
-    
+
 	return hotelsByCity, nil
 }
