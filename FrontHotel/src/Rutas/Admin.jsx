736ed983f57996f933cf320e3ffbde1179192bc8
Juanmairon <2100815@ucc.edu.ar>
2023-12-10 22:03:43 -0300

    Front de Admin aparentemente hecho

--- FrontHotel/src/Rutas/Admin.jsx
+++ FrontHotel/src/Rutas/Admin.jsx
@@ -1,61 +1,98 @@
-import React, { useState, useEffect } from "react"
-import '../Stylesheet/Admin.css'
-import Header from '../Componentes/Header'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import '../Stylesheet/Admin.css';
+import Header from '../Componentes/Header';
 
 function Admin() {
     const [estadisticas, setEstadisticas] = useState([]);
     const [serviciosEscalables, setServiciosEscalables] = useState([]);
-    const [contenedoresSeleccionados, setContenedoresSeleccionados] = useState([]);
     const usuarioValidado = localStorage.getItem('usuarioValidado');
+    const navigate = useNavigate();
+    const [eliminando, setEliminando] = useState(false);
+    const [escalando, setEscalando] = useState(false);
 
     useEffect(() => {
         if (!usuarioValidado) {
             navigate(`/home`);
         }
-        // Llamar a la API para obtener estadísticas cuando el componente se monta
-        fetch("http://localhost:8059/stats")  // Actualiza la URL con el puerto correcto
-            .then(response => response.json())
-            .then(data => setEstadisticas(data))
-            .catch(error => console.error("Error al obtener estadísticas:", error));
 
-        // Llamar a la API para obtener servicios escalables al cargar la página
-        fetch("http://localhost:8059/services")  // Actualiza la URL con el puerto correcto
+        cargarEstadisticas();
+
+        fetch("http://localhost:8059/services")
             .then(response => response.json())
             .then(data => setServiciosEscalables(data))
             .catch(error => console.error("Error al obtener servicios escalables:", error));
     }, []);
 
-    const handleCheckboxChange = (id) => {
-        // Actualizar la lista de contenedores seleccionados
-        setContenedoresSeleccionados((prevSelected) => {
-            if (prevSelected.has(id)) {
-                // Desmarcar el checkbox si ya está seleccionado
-                prevSelected.delete(id);
-            } else {
-                // Marcar el checkbox si no está seleccionado
-                prevSelected.add(id);
-            }
-            return new Set(prevSelected); // Garantizar la inmutabilidad
-        });
+    const handleCheckboxChange = (ID) => {
+        setEstadisticas(prevStats =>
+            prevStats.map(stat =>
+                stat.ID === ID ? { ...stat, isSelected: !stat.isSelected } : stat
+            )
+        );
+    };
+
+    const cargarEstadisticas = () => {
+        fetch("http://localhost:8059/stats")
+            .then(response => response.json())
+            .then(data => {
+                const sortedData = data.map(stat => ({ ...stat, isSelected: false }))
+                    .sort((a, b) => a.Name.localeCompare(b.Name));
+                setEstadisticas(sortedData);
+                setEliminando(false); // Cambiar estado eliminando a false después de cargar estadísticas
+            })
+            .catch(error => {
+                console.error("Error al obtener estadísticas:", error);
+                setEliminando(false); // Asegurarse de cambiar el estado incluso si hay un error
+            });
     };
 
     const handleDeleteSelectedContainers = () => {
-        // Llamar a la API para eliminar contenedores seleccionados
-        contenedoresSeleccionados.forEach((id) => {
-            fetch(`http://localhost:8059/container/${id}`, {
-                method: 'DELETE',
-            })
+        setEliminando(true); // Iniciar eliminación
+        const selectedIds = estadisticas
+            .filter(stat => stat.isSelected)
+            .map(stat => stat.ID);
+
+        Promise.all(selectedIds.map(ID =>
+            fetch(`http://localhost:8059/container/${ID}`, { method: 'DELETE' })
                 .then(response => response.json())
                 .then(data => {
-                    console.log(data);
-                    alert("Se han borrado los contenedores exitosamente");
-                    // Recargar la página después de borrar los contenedores
-                    window.location.reload();
+                    console.log(`Contenedor ${ID} eliminado:`, data);
+                    return ID;
                 })
-                .catch(error => console.error(`Error al eliminar contenedor ${id}:`, error));
+                .catch(error => {
+                    console.error(`Error al eliminar contenedor ${ID}:`, error);
+                    return null;
+                })
+        ))
+            .then(() => {
+                setTimeout(() => {
+                    cargarEstadisticas(); // Llamar a cargarEstadisticas después de 5 segundos
+                }, 30000);
+            });
+    };
+
+    const handleScaleService = (servicio) => {
+        setEscalando(true);
+        fetch("http://localhost:8059/scale", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ servicio })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(`Servicio ${servicio} escalado:`, data);
+            setTimeout(() => {
+                cargarEstadisticas();
+                setEscalando(false); // Finalizar el proceso de escalamiento
+            }, 5000);
+        })
+        .catch(error => {
+            console.error(`Error al escalar el servicio ${servicio}:`, error);
+            setEscalando(false); // Asegurarse de cambiar el estado incluso si hay un error
         });
-        // Limpiar la lista de contenedores seleccionados después de la eliminación
-        setContenedoresSeleccionados([]);
     };
 
     return (
@@ -74,7 +111,7 @@ function Admin() {
                 </thead>
                 <tbody>
                     {estadisticas.map(estadistica => (
-                        <tr key={estadistica.Id}>
+                        <tr key={estadistica.ID}>
                             <td>{estadistica.Name}</td>
                             <td>{estadistica.CPUPerc}</td>
                             <td>{estadistica.MemPerc}</td>
@@ -82,8 +119,8 @@ function Admin() {
                             <td>
                                 <input
                                     type="checkbox"
-                                    onChange={() => handleCheckboxChange(estadistica.Id)}
-                                    checked={contenedoresSeleccionados.has(estadistica.Id)}
+                                    onChange={() => handleCheckboxChange(estadistica.ID)}
+                                    checked={estadistica.isSelected}
                                 />
                             </td>
                         </tr>
@@ -91,18 +128,28 @@ function Admin() {
                 </tbody>
             </table>
 
-            <button onClick={handleDeleteSelectedContainers}>Borrar contenedores seleccionados</button>
+            <button className="boton-borrar-contenedores" onClick={handleDeleteSelectedContainers}>
+                Borrar contenedores seleccionados
+            </button>
+
+            {eliminando && <div>Eliminando contenedor...</div>}
 
             <div className="scalable-services-section">
                 <h3>Servicios Escalables</h3>
                 <ul>
                     {serviciosEscalables.map(servicio => (
-                        <li key={servicio}>{servicio}</li>
+                        <li key={servicio}>
+                            {servicio}
+                            <button onClick={() => handleScaleService(servicio)} disabled={escalando}>Escalar</button>
+                        </li>
                     ))}
                 </ul>
             </div>
+
+            {escalando && <div>Escalando servicio...</div>}
         </div>
     );
 }
 
 export default Admin;
+
